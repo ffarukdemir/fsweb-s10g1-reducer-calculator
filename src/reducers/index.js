c78f8e1cc7447142714e3f4d75e38738fb37ff03
Faruk Demir <farukdemir90@outlook.com>
2023-09-24 23:17:13 +0300

    hesap makinesi tamamlandı

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,47 +1,78 @@
-import { ADD_ONE, APPLY_NUMBER, CHANGE_OPERATION } from './../actions';
+import {
+  ADD_ONE,
+  APPLY_NUMBER,
+  CHANGE_OPERATION,
+  CLEAR_DISPLAY,
+  MEMORY_ADD,
+  MEMORY_CLEAR,
+  MEMORY_RECALL,
+} from "./../actions";
 
 export const initialState = {
-  total: 100,
-  operation: "*",
-  memory: 100
-}
+  total: 0,
+  operation: "+",
+  memory: 0,
+};
 
 const calculateResult = (num1, num2, operation) => {
   switch (operation) {
-    case ("+"):
+    case "+":
       return num1 + num2;
-    case ("*"):
+    case "*":
       return num1 * num2;
-    case ("-"):
+    case "-":
       return num1 - num2;
     default:
       return;
   }
-}
+};
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case (ADD_ONE):
-      return ({
+    case ADD_ONE:
+      return {
         ...state,
-        total: state.total + 1
-      });
+        total: state.total + 1,
+      };
 
-    case (APPLY_NUMBER):
-      return ({
+    case APPLY_NUMBER:
+      return {
         ...state,
-        total: calculateResult(state.total, action.payload, state.operation)
-      });
+        total: calculateResult(state.total, action.payload, state.operation),
+      };
 
-    case (CHANGE_OPERATION):
-      return ({
+    case CHANGE_OPERATION:
+      return {
         ...state,
-        operation: action.payload
-      });
+        operation: action.payload,
+      };
+    case CLEAR_DISPLAY:
+      return {
+        ...state,
+        total: initialState.total,
+      };
+
+    case MEMORY_ADD:
+      return {
+        ...state,
+        memory: state.total,
+      };
+
+    case MEMORY_CLEAR:
+      return {
+        ...state,
+        memory: initialState.memory,
+      };
+
+    case MEMORY_RECALL:
+      return {
+        ...state,
+        total: state.memory,
+      };
 
     default:
       return state;
   }
-}
+};
 
 export default reducer;
